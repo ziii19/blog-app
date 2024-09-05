b1ee24c579f31bf9ad5ad5b66f9f2aa89a17452e
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-05 10:46:48 +0700

    feat: set the global input theme and create auth field

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -2,7 +2,18 @@ import 'package:blog_app/core/theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  static _border([Color color = AppPallete.borderColor]) => OutlineInputBorder(
+        borderSide: BorderSide(
+          color: color,
+          width: 3,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      );
   static final darkThemeMode = ThemeData.dark().copyWith(
-    scaffoldBackgroundColor: AppPallete.backgroundColor
-  );
+      scaffoldBackgroundColor: AppPallete.backgroundColor,
+      inputDecorationTheme: InputDecorationTheme(
+        contentPadding: const EdgeInsets.all(27),
+        enabledBorder: _border(),
+        focusedBorder: _border(AppPallete.gradient1),
+      ));
 }
